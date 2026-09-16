60fb4ef9ff00b6873f9f2d1cc6cca63badaa2368
nischhelm <nischhelm@gmail.com>
2026-09-16 14:03:17 +0200

    make mixin compatible...

--- src/main/java/unhideflags/mixin/ItemStackMixin.java
+++ src/main/java/unhideflags/mixin/ItemStackMixin.java
@@ -1,23 +1,19 @@
 package unhideflags.mixin;
 
+import com.llamalad7.mixinextras.expression.Definition;import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.Objects;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
-
-    @Redirect(
-            method = "getTooltip",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NBTTagCompound;hasKey(Ljava/lang/String;I)Z")
-    )
-    public boolean unhideflags_vanillaEntityPlayer_attackEntityFrom(NBTTagCompound instance, String key, int type) {
-        if(Objects.equals(key, "HideFlags"))
-            return false;
-        return instance.hasKey(key,type);
+    @Expression("?.hasKey('HideFlags', 99)")
+    @Definition(id = "hasKey", method = "Lnet/minecraft/nbt/NBTTagCompound;hasKey(Ljava/lang/String;I)Z")
+    @WrapOperation(method = "getTooltip", at = @At(value = "MIXINEXTRAS:EXPRESSION"))
+    public boolean unhideflags_vanillaEntityPlayer_attackEntityFrom(NBTTagCompound instance, String key, int type, Operation<Boolean> original) {
+        return false;
     }
 }
