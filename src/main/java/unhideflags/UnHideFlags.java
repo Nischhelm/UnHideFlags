d79840ffb902c64e1201610691bc3cd16651d6e1
nischhelm <nischhelm@gmail.com>
2026-09-16 13:59:47 +0200

    just cosmetics

--- src/main/java/unhideflags/UnHideFlags.java
+++ src/main/java/unhideflags/UnHideFlags.java
@@ -5,7 +5,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = UnHideFlags.MODID, version = UnHideFlags.VERSION, name = UnHideFlags.NAME, dependencies = "required-after:fermiumbooter")
+@Mod(
+        modid = UnHideFlags.MODID,
+        version = UnHideFlags.VERSION,
+        name = UnHideFlags.NAME,
+        dependencies = "required-after:fermiumbooter@[1.3.2,)"
+)
 public class UnHideFlags {
     public static final String MODID = "unhideflags";
     public static final String VERSION = "1.0.0";
