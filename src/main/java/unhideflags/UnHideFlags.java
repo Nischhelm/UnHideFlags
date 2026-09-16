fbbf7a5388680f08166e805b3dcdcca06873c2bb
nischhelm <nischhelm@gmail.com>
2026-09-16 17:49:49 +0200

    v2.0.0
    - added default mode of scrambling instead of just unhiding, config toggleable
    - in that mode, hidden tooltips are readable for short periods between longer periods of being scrambled (TextFormatting.OBFUSCATED)
    - fixed mixin being potentially incompatible with other mixins touching itemstack tooltip code
    - now depends on fermiumbooter 1.3.2+ due to heavy use of mixinextras 0.5.0+

--- src/main/java/unhideflags/UnHideFlags.java
+++ src/main/java/unhideflags/UnHideFlags.java
@@ -15,7 +15,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class UnHideFlags {
     public static final String MODID = "unhideflags";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "2.0.0";
     public static final String NAME = "UnHideFlags";
     public static final Logger LOGGER = LogManager.getLogger();
 
